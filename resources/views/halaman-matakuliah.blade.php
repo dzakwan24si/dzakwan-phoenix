c645b46ca004b772fe57c5b6d8315d7062d00087
dzakwanpcr <dzakwan24si@mahasiswa.pcr.ac.id>
2025-09-23 07:51:50 +0700

    setup awal git

---
+++ resources/views/halaman-matakuliah.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>Selamat Datang!</h1>
+        <h2>Ini halaman Data Mata Kuliah</h2>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatakuliahController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pcr', function () {
+    return view('Selamat Datang di Website kampus PCR');
+});
+
+Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+
+Route::get('/nama/{param1}', function ($param1) {
+    return 'Nama saya: '.$param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: '.$param1;
+});
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
+
+Route::get('/matakuliah', [MatakuliahController::class, 'index']);
+Route::get('/matakuliah/show/', [MatakuliahController::class, 'show']);
+
