67c32e22c4e3305fb46ba4c11dabc3febf4efefd
dzakwan24si <dzakwan24si@mahasiswa.pcr.ac.id>
2025-09-30 10:54:02 +0700

    Menambahkan Error Messages

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -27,10 +27,24 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        $data['nama']   = $request->nama;
-        $data['email']   = $request->email;
-        $data['pertanyaan']   = $request->pertanyaan;
+        $request->validate([
+		    'nama'  => 'required|max:10',
+		    'email' => ['required','email'],
+		    'pertanyaan' => 'required|max:300|min:8',
+		],[
+            'nama.required'=>'Nama tidak boleh kosong',
+            'email.email'=>'Email tidak valid'
+        ]);
 
+        $request->validate([
+            'nama'  => 'required|max:10',
+            'email' => ['required','email'],
+            'pertanyaan' => 'required|max:300|min:8',
+        ]);
+
+        $data['nama'] = $request->nama;
+        $data['email'] = $request->email;
+        $data['pertanyaan'] = $request->pertanyaan;
         return view('home-question-respon', $data);
     }
 
